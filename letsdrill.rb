38e822b7b044e604a679b1749a46e519234f5b20
James O'Dwyer <j.odwyer320@gmail.com>
2014-09-13 09:49:18 -0400

    completed letsdrill challenge

--- letsdrill.rb
+++ letsdrill.rb
@@ -1,13 +1,29 @@
-def get_letter_grade(integer)
+def get_letter_grade(grade)
  
-  #Put your code here!
-
+  if grade >= 90
+  	return "A"
+  elsif grade >= 80
+  	return "B"
+  elsif grade >= 70
+  	return "C"
+  elsif grade >= 60
+  	return "D"
+  else
+  	return "F"
+  end
 end
 
 def shortest_string(array)
-
-  #Put your code here!
-
+	$short_string_count = 100
+	$short_string = nil
+	
+	array.each do |word|
+		if word.length < $short_string_count
+			$short_string_count = word.length
+			$short_string = word
+		end
+	end
+	return $short_string
 end
  
 
