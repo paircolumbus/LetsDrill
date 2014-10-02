ffe0e5d8e068fcaadd3bd8bb2ad97c6f6c69da64
Jennifer Cable <jcable@covermymeds.com>
2014-10-02 14:02:26 -0400

    made changes

--- letsdrill.rb
+++ letsdrill.rb
@@ -1,24 +1,40 @@
-def get_letter_grade(integer)
- 
-  #Put your code here!
+def get_letter_grade(score)
+
+  score = score.to_i
+  if score >= 90
+    "A"
+  elsif score >= 80
+    "B"
+  elsif score >= 70
+    "C"
+  elsif score >= 60
+    "D"
+  else
+    "F"
+  end
 
 end
+
+
+
 
 def shortest_string(array)
 
-  #Put your code here!
+array.min
+
+array.min { |a,b| a.length <=> b.length }
 
 end
- 
+
 
 
 ### Don't touch anything below this line ###
- 
+
 p "Fetch Letter Grade: You should have 2 trues"
-p get_letter_grade(89) == "B" 
-p get_letter_grade(73) == "C" 
+p get_letter_grade(89) == "B"
+p get_letter_grade(73) == "C"
 p
 p "Shortest String: You should have 3 trues"
 p shortest_string(["touch","this", "car"]) == "car"
-p shortest_string([]) == nil 
-p shortest_string(["can", "a", "solve", "Find", "solution"]) == "a" 
+p shortest_string([]) == nil
+p shortest_string(["can", "a", "solve", "Find", "solution"]) == "a"
