6ca538c70d3205e70ef303f437793a65cdf813cf
Peter Mueller <pmueller@covermymeds.com>
2015-05-22 11:05:29 -0400

    add logic for get_letter_grade, shortest_string

--- letsdrill.rb
+++ letsdrill.rb
@@ -1,24 +1,34 @@
 def get_letter_grade(integer)
- 
-  #Put your code here!
+  unless integer.class == Fixnum && integer.between?(0, 100)
+    raise ArgumentError.new("Not an integer between 0 and 100")
+  end
 
+  if integer < 60
+    "F"
+  elsif integer < 70
+    "D"
+  elsif integer < 80
+    "C"
+  elsif integer < 90
+    "B"
+  else
+    "A"
+  end
 end
 
 def shortest_string(array)
-
-  #Put your code here!
-
+  array.min_by { |x| x.length }
 end
- 
+
 
 
 ### Don't touch anything below this line ###
- 
+
 p "Fetch Letter Grade: You should have 2 trues"
-p get_letter_grade(89) == "B" 
-p get_letter_grade(73) == "C" 
+p get_letter_grade(89) == "B"
+p get_letter_grade(73) == "C"
 p
 p "Shortest String: You should have 3 trues"
 p shortest_string(["touch","this", "car"]) == "car"
-p shortest_string([]) == nil 
-p shortest_string(["can", "a", "solve", "Find", "solution"]) == "a" 
+p shortest_string([]) == nil
+p shortest_string(["can", "a", "solve", "Find", "solution"]) == "a"
