99722132d8f3d5d65163bb62fb47645d2fdd3463
Kaleb Davis <kdavis@covermymeds.com>
2015-06-03 12:46:48 -0400

    Revert "completed methods"
    This reverts commit 73c1a067d7d4b0df5af9a6f797c0344a98b31321.

--- letsdrill.rb
+++ letsdrill.rb
@@ -1,35 +1,24 @@
 def get_letter_grade(integer)
-
-  case integer
-	when 90..100
-		"A"
-	when 80..89
-		"B"
-	when 70..79
-    "C"
-	when 60..69
-		"D"
-	else
-		"F"
-	end
+ 
+  #Put your code here!
 
 end
 
 def shortest_string(array)
 
-  array.min_by(&:length)
+  #Put your code here!
 
 end
-
+ 
 
 
 ### Don't touch anything below this line ###
-
+ 
 p "Fetch Letter Grade: You should have 2 trues"
-p get_letter_grade(89) == "B"
-p get_letter_grade(73) == "C"
+p get_letter_grade(89) == "B" 
+p get_letter_grade(73) == "C" 
 p
 p "Shortest String: You should have 3 trues"
 p shortest_string(["touch","this", "car"]) == "car"
-p shortest_string([]) == nil
-p shortest_string(["can", "a", "solve", "Find", "solution"]) == "a"
+p shortest_string([]) == nil 
+p shortest_string(["can", "a", "solve", "Find", "solution"]) == "a" 
