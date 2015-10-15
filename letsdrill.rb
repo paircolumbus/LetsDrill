76f29f4d190f090756a7d778551dce4768f18854
Nick Legg <nlegg@covermymeds.com>
2015-10-15 19:45:55 -0400

    Fixed mixed up spacing (again)

--- letsdrill.rb
+++ letsdrill.rb
@@ -1,16 +1,16 @@
 def get_letter_grade(integer)
  
   case integer
-  when 90..100
-    "A"
-  when 80...90
-    "B"
-  when 70...80
-    "C"
-  when 60...70
-    "D"
-  else
-    "F"
+    when 90..100
+      "A"
+    when 80...90
+      "B"
+    when 70...80
+      "C"
+    when 60...70
+      "D"
+    else
+      "F"
   end
 
 end
