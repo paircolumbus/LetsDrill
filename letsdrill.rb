ce5af7a3637359519dfbf4abc16f505731fc9024
omro20 <orivort@gmail.com>
2015-11-15 22:53:54 -0500

    added letsdrill.rb file

--- letsdrill.rb
+++ letsdrill.rb
@@ -1,24 +1,37 @@
 def get_letter_grade(integer)
- 
+
   #Put your code here!
+  case integer
+  when 90..100
+    return "A"
+  when 80..89
+    return "B"
+  when 70..79
+    return "C"
+  when 60..69
+    return "D"
+  else
+    return "F"
+  end
 
 end
 
 def shortest_string(array)
 
   #Put your code here!
+  array.min { |a, b|  a.size <=> b.size  }
 
 end
- 
+
 
 
 ### Don't touch anything below this line ###
- 
+
 p "Fetch Letter Grade: You should have 2 trues"
-p get_letter_grade(89) == "B" 
-p get_letter_grade(73) == "C" 
+p get_letter_grade(89) == "B"
+p get_letter_grade(73) == "C"
 p
 p "Shortest String: You should have 3 trues"
 p shortest_string(["touch","this", "car"]) == "car"
-p shortest_string([]) == nil 
-p shortest_string(["can", "a", "solve", "Find", "solution"]) == "a" 
+p shortest_string([]) == nil
+p shortest_string(["can", "a", "solve", "Find", "solution"]) == "a"
