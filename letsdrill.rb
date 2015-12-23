784a3a0c5f5810ed8f60ed941d0713a5341b2ec0
davidslee101 <davidslee101@gmail.com>
2015-12-23 15:26:04 -0500

    add letter grade method

--- letsdrill.rb
+++ letsdrill.rb
@@ -1,7 +1,18 @@
 def get_letter_grade(integer)
- 
   #Put your code here!
-
+  if integer >= 90
+    puts "A"
+  elsif (integer >= 80)
+    puts "B"
+  elsif (integer >= 70)
+    puts "C"
+  elsif (integer >= 60)
+    puts "D"
+  elsif (integer >= 0)
+    puts "F"
+  else
+    puts "that is not a valid percentage number"
+  end
 end
 
 def shortest_string(array)
@@ -9,16 +20,16 @@ def shortest_string(array)
   #Put your code here!
 
 end
- 
+
 
 
 ### Don't touch anything below this line ###
- 
+
 p "Fetch Letter Grade: You should have 2 trues"
-p get_letter_grade(89) == "B" 
-p get_letter_grade(73) == "C" 
+p get_letter_grade(89) == "B"
+p get_letter_grade(73) == "C"
 p
 p "Shortest String: You should have 3 trues"
 p shortest_string(["touch","this", "car"]) == "car"
-p shortest_string([]) == nil 
-p shortest_string(["can", "a", "solve", "Find", "solution"]) == "a" 
+p shortest_string([]) == nil
+p shortest_string(["can", "a", "solve", "Find", "solution"]) == "a"
