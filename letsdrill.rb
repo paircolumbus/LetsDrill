5ef3e93816d95b6469e1e7a1b7a22f04d343c21c
nate anderson <nathanaelcanderson@gmail.com>
2016-01-11 21:31:18 -0500

    initial commit

--- letsdrill.rb
+++ letsdrill.rb
@@ -1,17 +1,25 @@
 def get_letter_grade(integer)
- 
-  #Put your code here!
-
+  if integer < 60
+    return "F"
+  elsif integer >= 60 && integer < 70
+  	return "D"
+  elsif integer >= 70 && integer < 80
+  	return "C"
+  elsif integer >= 80 && integer < 90
+  	return "B"
+  else 
+  	return "A"
+  end
 end
 
 def shortest_string(array)
-
-  #Put your code here!
-
+  string_length = array.sort_by { |word| word.length }
+  string_length.reverse
+  return string_length[0]
 end
  
 
-
+# =begin
 ### Don't touch anything below this line ###
  
 p "Fetch Letter Grade: You should have 2 trues"
@@ -22,3 +30,5 @@ p "Shortest String: You should have 3 trues"
 p shortest_string(["touch","this", "car"]) == "car"
 p shortest_string([]) == nil 
 p shortest_string(["can", "a", "solve", "Find", "solution"]) == "a" 
+
+# =end
