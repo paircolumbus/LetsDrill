8ed611fa215a09bae31ed72d26cfe65452075c99
Mike Earley <mearley@covermymeds.com>
2016-03-01 13:38:02 -0500

    Revert "mike's solutions for lets drill"
    This reverts commit ababfa36b3a9e8a516c7090b3c25007982558264.

--- letsdrill.rb
+++ letsdrill.rb
@@ -1,20 +1,12 @@
 def get_letter_grade(integer)
-	@integer = integer
  
-  case
-	  when integer.between?(90,100) then return "A"
-	  when integer.between?(80,89) then return "B"
-	  when integer.between?(70,79) then return "C"
-	  when integer.between?(60,69) then return "D"
-	  when integer < 59 then return "F"
-	  else return "You entered an invalid value"
-  end
+  #Put your code here!
 
 end
 
 def shortest_string(array)
 
-  array.min{ |a,b| a.size <=> b.size }
+  #Put your code here!
 
 end
  
