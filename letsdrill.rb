e851bbcbf5f42d7ca861f3b46d7752fa704bab16
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-03-09 09:43:57 -0500

    complete letsdrill

--- letsdrill.rb
+++ letsdrill.rb
@@ -1,24 +1,27 @@
 def get_letter_grade(integer)
- 
-  #Put your code here!
-
+  if integer >= 90 then 'A'
+  elsif integer >= 80 then 'B'
+  elsif integer >= 70 then 'C'
+  elsif integer >= 60 then 'D'
+  elsif integer >= 50 then 'F'
+  else 'Invalid grade'
+  end
 end
 
 def shortest_string(array)
-
-  #Put your code here!
-
+  array.sort { |x,y| x.length <=> y.length }.shift
 end
- 
+
 
 
 ### Don't touch anything below this line ###
- 
+
 p "Fetch Letter Grade: You should have 2 trues"
-p get_letter_grade(89) == "B" 
-p get_letter_grade(73) == "C" 
+p get_letter_grade(89) == "B"
+p get_letter_grade(73) == "C"
 p
+
 p "Shortest String: You should have 3 trues"
 p shortest_string(["touch","this", "car"]) == "car"
-p shortest_string([]) == nil 
-p shortest_string(["can", "a", "solve", "Find", "solution"]) == "a" 
+p shortest_string([]) == nil
+p shortest_string(["can", "a", "solve", "Find", "solution"]) == "a"
