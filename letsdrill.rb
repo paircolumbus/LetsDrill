bd178278aab3c3491a1f394726035868b8ebfbe1
Adam Harrison <aharrison@covermymeds.com>
2016-03-16 12:44:43 -0400

    prunetracy's solution

--- letsdrill.rb
+++ letsdrill.rb
@@ -1,13 +1,18 @@
 def get_letter_grade(integer)
- 
-  #Put your code here!
-
+  if integer >= 90 
+    "A"
+  elsif integer >= 80 
+    "B"
+  elsif integer >= 70 
+    "C"
+  elsif integer >= 60 
+    "D"
+  else "F"
+  end
 end
 
 def shortest_string(array)
-
-  #Put your code here!
-
+  array.sort {|a, b| a.length <=> b.length}.shift
 end
  
 
