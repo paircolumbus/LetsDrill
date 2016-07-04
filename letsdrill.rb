7b13a63f4dff0c3acf78a7a4e8d46cf340be8989
Liz Cymanski <lcymanski@CMPCMOSXLT006.local>
2016-07-04 09:39:42 -0400

    working

--- letsdrill.rb
+++ letsdrill.rb
@@ -1,13 +1,39 @@
 def get_letter_grade(integer)
- 
-  #Put your code here!
+=begin
+	grade="A"
+	if integer<60
+		grade ="F"
+	elsif integer<70
+		grade ="D"
+	elsif integer < 80
+		grade = "C"
+	elsif integer <90
+		grade="B"
+	end	
+	return grade
+=end
+
+	return case integer
+	when 0..59
+		"F"
+	when 60..69
+		"D"
+	when 70..79
+		"C"
+	when 80..89
+		"B"
+	else
+		"A"
+	end
 
 end
 
 def shortest_string(array)
-
-  #Put your code here!
-
+	short = array.first
+	array.each do |word|
+		short=word if word.length<short.length
+	end
+	return short 
 end
  
 
