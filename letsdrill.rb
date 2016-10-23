f30bb1a7abcdb54d5cf02f828be5fc6070454b1c
Drew T. Ritcher <ritchedt@miamioh.edu>
2016-10-23 14:26:58 -0400

    Finished Challenge -- LetsDrill
    Done!

--- letsdrill.rb
+++ letsdrill.rb
@@ -1,24 +1,40 @@
 def get_letter_grade(integer)
- 
-  #Put your code here!
 
+  grade = case integer
+    when 90..100 then "A"
+    when 80..89 then "B"
+    when 70..79 then "C"
+    when 60..69 then "D"
+    when 0..59 then "F"
+    else "Invalid Score"
+  end
+
+  return grade
 end
 
 def shortest_string(array)
 
-  #Put your code here!
+  if array.length == 0
+    return nil
+  end
 
+  index = array[0]
+  array.each { |x|
+    if x.length < index.length
+      index = x
+    end
+  }
+  return index
 end
- 
 
 
 ### Don't touch anything below this line ###
- 
+
 p "Fetch Letter Grade: You should have 2 trues"
-p get_letter_grade(89) == "B" 
-p get_letter_grade(73) == "C" 
+p get_letter_grade(89) == "B"
+p get_letter_grade(73) == "C"
 p
 p "Shortest String: You should have 3 trues"
 p shortest_string(["touch","this", "car"]) == "car"
-p shortest_string([]) == nil 
-p shortest_string(["can", "a", "solve", "Find", "solution"]) == "a" 
+p shortest_string([]) == nil
+p shortest_string(["can", "a", "solve", "Find", "solution"]) == "a"
