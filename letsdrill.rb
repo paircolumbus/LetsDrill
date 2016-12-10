fbf46fcbc395dce5a898817e9d54874acf0da454
Jylbeya <jylkasonga@hotmail.com>
2016-12-10 17:49:03 -0500

    Challenge let's Drill

--- letsdrill.rb
+++ letsdrill.rb
@@ -1,15 +1,23 @@
 def get_letter_grade(integer)
- 
   #Put your code here!
-
+	if integer >= 90
+		return  "A"
+	elsif integer >=80
+		return "B"
+	elsif integer >=70
+		return "C"
+	elsif integer >=60
+		return "D"
+	else
+		return "F"
+	end
 end
 
 def shortest_string(array)
 
   #Put your code here!
-
+  return array.sort_by{|n|n.length}[0]
 end
- 
 
 
 ### Don't touch anything below this line ###
