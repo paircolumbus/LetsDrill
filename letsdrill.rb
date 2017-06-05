3097fbdae8023e620ab25e8d9bd23f2d09c07521
Matthew Crocco <matthewcrocco@gmail.com>
2017-06-04 22:49:16 -0400

    Solution commit

--- letsdrill.rb
+++ letsdrill.rb
@@ -1,32 +1,17 @@
 
-def compare_word_length_pairs(a, b)
-  a.last <=> b.last
-end
-
-# @param [Integer] number_grade
 def get_letter_grade(number_grade)
-
-  grade_ranges = {
-      90..100 => 'A',
-      80..89  => 'B',
-      70..79  => 'C',
-      60..69  => 'D',
-       0..59  => 'F',
-  }
-
-  # Selects range that includes the given number grade
-  grade_ranges.select { |grade| grade.include? number_grade }.values.first
+  case number_grade
+    when 90..100 then 'A'
+    when 80...90 then 'B'
+    when 70...80 then 'C'
+    when 60...70 then 'D'
+    else 'F'
+  end
 end
 
-# @param [Array<String>] words
 def shortest_string(words)
-  if words.length == 0
-    nil
-  else
-    lengths = words.map { |s| s.length }
-
-    words_and_lengths = words.zip(lengths).sort! { |a, b| compare_word_length_pairs(a, b) }
-    words_and_lengths[0].first
+  unless words.length == 0
+    words.min_by &:length
   end
 end
 
@@ -34,7 +19,8 @@ end
  
 p "Fetch Letter Grade: You should have 2 trues"
 p get_letter_grade(89) == "B" 
-p get_letter_grade(73) == "C" 
+p get_letter_grade(73) == "C"
+p get_letter_grade(90) == "A"
 p
 p "Shortest String: You should have 3 trues"
 p shortest_string(["touch","this", "car"]) == "car"
