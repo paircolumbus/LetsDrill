ad873bd46ca472d6a071b5fa0ce87607f943283a
David Hatten <dhatten@cmmc02vt0hzg8wl.innova.local>
2018-01-18 14:56:55 -0500

    Fixing tests after forking to my own repo

--- letsdrill.rb
+++ letsdrill.rb
@@ -1,24 +1,41 @@
 def get_letter_grade(integer)
- 
-  #Put your code here!
+  result = ""
+  if integer > 89
+    result = "A"
+  elsif integer > 79
+    result = "B"
+  elsif integer > 69
+    result = "C"
+  elsif integer > 59
+    result = "D"
+  else
+    result = "F"
+  end
 
+  return result
 end
 
 def shortest_string(array)
+  result = array.first
 
-  #Put your code here!
+  array.each { |item|
+    if item.length < result.length
+      result = item
+    end
+  }
 
+  return result
 end
- 
+
 
 
 ### Don't touch anything below this line ###
- 
+
 p "Fetch Letter Grade: You should have 2 trues"
-p get_letter_grade(89) == "B" 
-p get_letter_grade(73) == "C" 
+p get_letter_grade(89) == "B"
+p get_letter_grade(73) == "C"
 p
 p "Shortest String: You should have 3 trues"
 p shortest_string(["touch","this", "car"]) == "car"
-p shortest_string([]) == nil 
-p shortest_string(["can", "a", "solve", "Find", "solution"]) == "a" 
+p shortest_string([]) == nil
+p shortest_string(["can", "a", "solve", "Find", "solution"]) == "a"
