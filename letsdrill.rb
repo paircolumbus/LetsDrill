ffbf5d5477ead66652a150856e34c57b2c498e60
Andrew Bonner <abonner@cmmc02q32gzg8wl.innova.local>
2018-05-10 13:02:15 -0400

    Pass letter grade tests.

--- letsdrill.rb
+++ letsdrill.rb
@@ -1,7 +1,15 @@
 def get_letter_grade(integer)
- 
-  #Put your code here!
-
+  if integer >= 90
+    "A"
+  elsif integer < 90 && integer >= 80
+    "B"
+  elsif integer < 80 && integer >= 70
+    "C"
+  elsif integer < 70 && integer >= 60
+    "D"
+  else
+    "F"
+  end
 end
 
 def shortest_string(array)
@@ -9,16 +17,16 @@ def shortest_string(array)
   #Put your code here!
 
 end
- 
+
 
 
 ### Don't touch anything below this line ###
- 
+
 p "Fetch Letter Grade: You should have 2 trues"
-p get_letter_grade(89) == "B" 
-p get_letter_grade(73) == "C" 
+p get_letter_grade(89) == "B"
+p get_letter_grade(73) == "C"
 p
 p "Shortest String: You should have 3 trues"
 p shortest_string(["touch","this", "car"]) == "car"
-p shortest_string([]) == nil 
-p shortest_string(["can", "a", "solve", "Find", "solution"]) == "a" 
+p shortest_string([]) == nil
+p shortest_string(["can", "a", "solve", "Find", "solution"]) == "a"
