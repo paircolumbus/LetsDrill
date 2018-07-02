4d368e67064696db2409c09c242231b868e71c1f
AlexRichards9595 <arichards@netjets.com>
2018-07-02 12:39:28 -0400

    Initial commit

--- letsdrill.rb
+++ letsdrill.rb
@@ -1,5 +1,5 @@
 def get_letter_grade(integer)
- 
+
   #Put your code here!
 
 end
@@ -7,18 +7,19 @@ end
 def shortest_string(array)
 
   #Put your code here!
+  #Initial Commit
 
 end
- 
+
 
 
 ### Don't touch anything below this line ###
- 
+
 p "Fetch Letter Grade: You should have 2 trues"
-p get_letter_grade(89) == "B" 
-p get_letter_grade(73) == "C" 
+p get_letter_grade(89) == "B"
+p get_letter_grade(73) == "C"
 p
 p "Shortest String: You should have 3 trues"
 p shortest_string(["touch","this", "car"]) == "car"
-p shortest_string([]) == nil 
-p shortest_string(["can", "a", "solve", "Find", "solution"]) == "a" 
+p shortest_string([]) == nil
+p shortest_string(["can", "a", "solve", "Find", "solution"]) == "a"
