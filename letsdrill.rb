e227a9e12c4b1542c23520b4ad3e5d009121fad2
matthew lee <matthewlee07@gmail.com>
2018-09-26 15:37:22 -0400

    successfully completed

--- letsdrill.rb
+++ letsdrill.rb
@@ -1,19 +1,28 @@
 def get_letter_grade(integer)
- 
-  #Put your code here!
-
+  case integer
+  when 91..100
+    "A"
+  when 81..90
+    "B"
+  when 71..80
+    "C"
+  when 61..70
+    "D"
+  when integer<60
+    "F"
+  else
+    "Invalid Input"
+  end
 end
 
 def shortest_string(array)
-
-  #Put your code here!
-
+  array.sort_by {|string| string.length}[0]
 end
- 
+
 
 
 ### Don't touch anything below this line ###
- 
+
 p "Fetch Letter Grade: You should have 2 trues"
 p get_letter_grade(89) == "B" 
 p get_letter_grade(73) == "C" 
