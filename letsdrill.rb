dbcb106e754fc7778d73d746afa1cef7968456ec
Evan McCullough <evan.m.mccullough@gmail.com>
2019-05-24 10:14:09 -0400

    Evan McCullough's solutions to the LetsDrill Challenge

--- letsdrill.rb
+++ letsdrill.rb
@@ -1,24 +1,41 @@
 def get_letter_grade(integer)
- 
-  #Put your code here!
-
+  if integer > 89
+    'A'
+  elsif integer > 79 # Using just > here, since this elsif implies already being <= 89
+    'B'
+  elsif integer > 69 # Using just > here, since this elsif implies already being <= 79
+    'C'
+  elsif integer > 59
+    'D'
+  else
+    'F'
+  end
 end
 
 def shortest_string(array)
-
-  #Put your code here!
+  min_string = array.join
+  if min_string.length == 0
+    return nil
+  else
+    array.each do |str|
+      if str.length < min_string.length
+        min_string = str
+      end
+    end
+    return min_string
+  end
 
 end
- 
+
 
 
 ### Don't touch anything below this line ###
- 
+
 p "Fetch Letter Grade: You should have 2 trues"
-p get_letter_grade(89) == "B" 
-p get_letter_grade(73) == "C" 
+p get_letter_grade(89) == "B"
+p get_letter_grade(73) == "C"
 p
 p "Shortest String: You should have 3 trues"
 p shortest_string(["touch","this", "car"]) == "car"
-p shortest_string([]) == nil 
-p shortest_string(["can", "a", "solve", "Find", "solution"]) == "a" 
+p shortest_string([]) == nil
+p shortest_string(["can", "a", "solve", "Find", "solution"]) == "a"
